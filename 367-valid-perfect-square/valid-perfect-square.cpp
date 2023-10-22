c5a8f5272d2d4412e221606140d8ed69ea93a27a
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2023-10-22 18:01:42 +0530

    Time: 2 ms (56.55%) | Memory: 6.2 MB (43.10%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    bool isPerfectSquare(int num) {
+        long long low = 1, high = num;
+        while(low <= high) {
+            long long mid = low + (high - low) / 2;
+            if(mid * mid == num) {
+                return true;
+            } else if(mid * mid < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    // Approach 1: O(sqrt(n))
+    // bool isPerfectSquare(int num) {
+    //     double sqt = sqrt(num);
+    //     return ceil(sqt) == floor(sqt);
+    // }
+};
