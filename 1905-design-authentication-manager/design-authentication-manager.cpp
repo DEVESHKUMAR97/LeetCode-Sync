f8674dd377a2f05f9efd6ecf9809263b58bcba18
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2023-10-31 16:45:21 +0530

    Time: 67 ms (79.18%) | Memory: 32.2 MB (9.23%) - LeetSync

--- 1905-design-authentication-manager/design-authentication-manager.cpp
+++ 1905-design-authentication-manager/design-authentication-manager.cpp
@@ -1,56 +1,46 @@
 class AuthenticationManager {
-
-private:
-    struct compare
-    {
-        bool operator()(pair<string, int> p1, pair<string, int> p2)
-        {
-            return p1.second > p2.second;
-        }
-    };
-    
-    int ttl;
-    unordered_map<string, int> ump;
-    priority_queue<pair<string, int>, vector<pair<string, int>>, compare> pq;
-
 public:
-    AuthenticationManager(int timeToLive) 
-    {
-        ttl = timeToLive;
-    }
+    int TTL; // time to live
+    unordered_map<string, int> tokens; // tokenId -> exp time
+    multimap<int, string> time2tokens; // exp time -> tokenId
 
-    void generate(string tokenId, int currentTime) 
-    {
-        ump[tokenId] = currentTime + ttl;
-        pq.push({tokenId, currentTime + ttl});
+    AuthenticationManager(int timeToLive) {
+        TTL = timeToLive;
     }
+    
+    void generate(string tokenId, int currentTime) {
+        // O(logn)
+        tokens[tokenId] = currentTime + TTL;
+        time2tokens.insert({currentTime + TTL, tokenId});
+    }
+    
+    void renew(string tokenId, int currentTime) {
+        // O(logn)
+        if(tokens.count(tokenId) == 0) return;
+        if(tokens[tokenId] <= currentTime) tokens.erase(tokenId);
+        else generate(tokenId, currentTime);
+    }
+    
+    int countUnexpiredTokens(int currentTime) {
+        // O(logn)
+        auto end_it = time2tokens.upper_bound(currentTime);
 
-    void renew(string tokenId, int currentTime) 
-    {
-        if(ump.find(tokenId) != ump.end() && ump[tokenId] > currentTime)
-        {
-            ump[tokenId] = currentTime + ttl;
+        // Remove expired tokens in hash map
+        for(auto it = time2tokens.begin() ; it != end_it ; ++it){
+            if(tokens.count(it->second) && tokens[it->second] == it->first) 
+                tokens.erase(it->second);
         }
-    }
+ 
+        time2tokens.erase(time2tokens.begin(), end_it);
 
-    int countUnexpiredTokens(int currentTime) 
-    {
-        while (!pq.empty()) 
-        {
-            int originalExpiryTime = pq.top().second;
-            string tokenId = pq.top().first;
-            if (originalExpiryTime > currentTime)
-            {
-                break;
-            }
-
-            pq.pop();
-            int actualExpiryTime = ump[tokenId];
-            if (actualExpiryTime > currentTime)
-            {
-                pq.push({tokenId,actualExpiryTime});
-            }
-        }
-        return pq.size();
+        return tokens.size();
     }
 };
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager* obj = new AuthenticationManager(timeToLive);
+ * obj->generate(tokenId,currentTime);
+ * obj->renew(tokenId,currentTime);
+ * int param_3 = obj->countUnexpiredTokens(currentTime);
+ */
