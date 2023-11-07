22b8729e5adb7692a4089054f138d70974992224
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2023-11-07 12:38:59 +0530

    Time: 87 ms (98.67%) | Memory: 83.4 MB (74.13%) - LeetSync

--- 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
@@ -3,24 +3,44 @@ public:
 
     // approach 1: sorting
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
-        vector<float> arrival;
+        int n = dist.size();
+        vector<int> arrivals(n+1, 0);
         for (int i = 0; i < dist.size(); i++) {
-            arrival.push_back((float) dist[i] / speed[i]);
+            int time = ceil((float) dist[i] / speed[i]);
+            if(time > n) time = n;
+            arrivals[time]++;
         }
         
-        sort(arrival.begin(), arrival.end());
-        int ans = 0;
-        
-        for (int i = 0; i < arrival.size(); i++) {
-            // cout << arrival[i] << " ";
-            if (arrival[i] <= i) {
-                // cout << "Hello\n";
-                break;
-            }
-            
-            ans++;
+        int shot = 0;
+        int monsters = 0;
+        for (shot = 0; shot < n; shot++) {
+            monsters += arrivals[shot];
+            if(monsters > shot) break;
         }
         
-        return ans;
+        return shot;
     }
+
+    // // approach 1: sorting
+    // int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+    //     vector<float> arrival;
+    //     for (int i = 0; i < dist.size(); i++) {
+    //         arrival.push_back((float) dist[i] / speed[i]);
+    //     }
+        
+    //     sort(arrival.begin(), arrival.end());
+    //     int ans = 0;
+        
+    //     for (int i = 0; i < arrival.size(); i++) {
+    //         // cout << arrival[i] << " ";
+    //         if (arrival[i] <= i) {
+    //             // cout << "Hello\n";
+    //             break;
+    //         }
+            
+    //         ans++;
+    //     }
+        
+    //     return ans;
+    // }
 };
