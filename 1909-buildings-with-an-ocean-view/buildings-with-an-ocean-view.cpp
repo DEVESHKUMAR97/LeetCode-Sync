7509a76445a23c8162a4072e1e3fa45005c5a779
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2024-05-02 22:02:19 +0530

    Time: 107 ms (30.02%) | Memory: 71.2 MB (36.44%) - LeetSync

---
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+        int n = heights.size();
+        int mx = -1;
+        vector<int> ans;
+        for(int i = n - 1; i >= 0; i--) {
+            if(heights[i] > mx) {
+                ans.push_back(i);
+            }
+            mx = max(mx, heights[i]);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+    }
+};
