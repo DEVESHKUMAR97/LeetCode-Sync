2a064c21534c1d473e4e903e0003b712b5d29aca
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2024-06-26 00:48:12 +0530

    Time: 27 ms (52.43%) | Memory: 33.1 MB (37.79%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void reverseInOrderTraversal(TreeNode* root, int& sum) {
+        if(root == nullptr) return;
+        reverseInOrderTraversal(root->right, sum);
+        sum += root->val;
+        root->val = sum;
+        reverseInOrderTraversal(root->left, sum);
+    }
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        int sum = 0;
+        reverseInOrderTraversal(root, sum);
+        return root;
+    }
+};
