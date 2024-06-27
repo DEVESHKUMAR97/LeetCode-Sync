34e201526f30fa99f2b5409a315f77cf327b7868
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2024-06-27 15:09:38 +0530

    Time: 5 ms (39.74%) | Memory: 8.2 MB (72.25%) - LeetSync

--- 2406-decode-the-message/decode-the-message.cpp
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        unordered_map<char, char> mp;
+        vector<char> mp(26, 0);
         char currChar = 'a';
         for(char ch: key) {
             if(ch != ' ') {
-                if(!mp.count(ch)) {
-                    mp[ch] = currChar++;
+                if(mp[ch-'a'] == 0) {
+                    mp[ch-'a'] = currChar++;
                 }
             }
         }
@@ -14,7 +14,7 @@ public:
         string ans = "";
         for(char ch: message) {
             if(ch != ' ') {
-                ans += mp[ch];
+                ans += mp[ch-'a'];
             } else {
                 ans += ' ';
             }
@@ -22,4 +22,26 @@ public:
         return ans;
         
     }
+    // string decodeMessage(string key, string message) {
+    //     unordered_map<char, char> mp;
+    //     char currChar = 'a';
+    //     for(char ch: key) {
+    //         if(ch != ' ') {
+    //             if(!mp.count(ch)) {
+    //                 mp[ch] = currChar++;
+    //             }
+    //         }
+    //     }
+
+    //     string ans = "";
+    //     for(char ch: message) {
+    //         if(ch != ' ') {
+    //             ans += mp[ch];
+    //         } else {
+    //             ans += ' ';
+    //         }
+    //     }
+    //     return ans;
+        
+    // }
 };
