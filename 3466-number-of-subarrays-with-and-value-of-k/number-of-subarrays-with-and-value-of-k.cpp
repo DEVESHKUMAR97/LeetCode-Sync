d0182daec00673ca3f9b8eb73ec8774c65bf17e2
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2024-07-09 20:31:46 +0530

    Time: 178 ms (98.63%) | Memory: 93.8 MB (99.23%) - LeetSync

--- 3466-number-of-subarrays-with-and-value-of-k/number-of-subarrays-with-and-value-of-k.cpp
+++ 3466-number-of-subarrays-with-and-value-of-k/number-of-subarrays-with-and-value-of-k.cpp
@@ -8,7 +8,7 @@ public:
 
         ll ans = 0;
         for(int i = 0; i < n; i++) {
-            int l = 0, r = i;
+            int l = -1, r = i;
             int num = nums[i];
             for(int bit = 0; bit < maxBit; bit++) {
                 if((num & (1 << bit)) == 0) {
@@ -16,13 +16,13 @@ public:
                 }
                 
                 if(k & (1 << bit)) { // 1
-                    l = max(l, lastZero[bit] + 1);
+                    l = max(l, lastZero[bit]);
                 } else {
                     r = min(r, lastZero[bit]);
                 }
             }
             if(r >= l)
-                ans += (r - l + 1);
+                ans += (r - l);
         }
         return ans;
     }
