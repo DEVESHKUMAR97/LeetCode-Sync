8cecc57b80e88d76c2b7ad39a5fca1464d6277fd
DEVESH KUMAR <35339342+DEVESHKUMAR97@users.noreply.github.com>
2024-07-25 21:29:14 +0530

    Time: 48 ms (79.11%) | Memory: 48.7 MB (69.32%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    let obj = {};
+    obj.toBe = (val2) => {
+        if(val === val2) return true;
+        throw "Not Equal";
+    }
+    obj.notToBe = (val2) => {
+        if(val !== val2) return true;
+        throw "Equal";
+    }
+    return obj;
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
